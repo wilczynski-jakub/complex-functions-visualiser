73fb8853e572db269d6462855603a1aaf1d4a730
Jakub Wilczyński <j.wilczynski2@student.uw.edu.pl>
2024-04-11 19:40:54 +0200

    Add files

---
+++ src/maths/ComplexFunction.java
@@ -0,0 +1,11 @@
+//ComplexFunction.java
+/** An interface to represent complex functions as lambda expressions.
+ *@author Jakub Wilczyński
+ *@since 13.11.2021
+ */
+
+package maths;
+
+public interface ComplexFunction {
+    ComplexNumber calculate(ComplexNumber input);
+}
